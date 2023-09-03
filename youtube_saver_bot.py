e8064ff1c2eca7bfa14537d0e373b913487c56a4
andrei <ssignal.fills.the.void@gmail.com>
2023-09-03 13:04:13 +0300

    бот, начало

---
+++ youtube_saver_bot.py
@@ -0,0 +1,38 @@
+from aiogram import Bot, executor, Dispatcher, types
+from config import TOKEN_API
+from pytube import YouTube
+import pytube
+import os
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def get_url(message: types.Message):
+    await bot.send_sticker(chat_id=message.chat.id,
+                           sticker='CAACAgQAAxkBAAEKJWVk7ExZWiHvnJkiCkR96QGIM0e1KwACQwgAArdkcVPkfM0BRtGhHzAE')
+    get_audio(message.text)
+
+
+def get_audio(url):
+    if get_video_duration(url) < 600:
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
+        out_file = video.download(output_path=".")
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print('success')
+    else:
+        print('too long')
+
+
+def get_video_duration(url):
+    youtube = pytube.YouTube(url)
+    duration = youtube.length
+    return duration
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
